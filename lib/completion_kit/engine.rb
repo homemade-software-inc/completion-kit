3f76698955dceb0fdcacd5965a8401d76cdc580a
Damien Bastin <damien.bastin@gmail.com>
2026-03-10 23:45:51 -0700

    feat: add vanilla JS for evaluation steps add/remove

--- lib/completion_kit/engine.rb
+++ lib/completion_kit/engine.rb
@@ -5,7 +5,7 @@ module CompletionKit
     paths.add "app/services", eager_load: true
 
     def self.register_assets(app)
-      app.config.assets.precompile += %w( completion_kit/application.css )
+      app.config.assets.precompile += %w( completion_kit/application.css completion_kit/evaluation_steps_controller.js )
     end
 
     initializer("completion_kit.assets") { |app| Engine.register_assets(app) }
