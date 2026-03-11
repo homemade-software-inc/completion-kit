aa62332f849e53b75992646fba1ca351a7bc0305
Damien Bastin <damien.bastin@gmail.com>
2026-03-10 23:33:14 -0700

    feat: update assessment factory for star rubric

--- spec/factories/test_result_metric_assessments.rb
+++ spec/factories/test_result_metric_assessments.rb
@@ -3,10 +3,10 @@ FactoryBot.define do
     association :test_result, factory: :completion_kit_test_result
     association :metric, factory: :completion_kit_metric
     metric_name { metric.name }
-    guidance_text { metric.guidance_text }
-    rubric_text { metric.rubric_text }
+    criteria { metric.criteria }
+    rubric_text { metric.display_rubric_text }
     status { "evaluated" }
-    ai_score { 8.5 }
+    ai_score { 4.5 }
     ai_feedback { "Strong match for the metric." }
   end
 end
