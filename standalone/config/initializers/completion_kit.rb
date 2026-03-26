106d805d00c8475c7a3ed451cf843aa303d9a44e
Damien Bastin <damien.bastin@gmail.com>
2026-03-26 12:23:53 -0700

    fix: only set auth credentials when password is configured

--- standalone/config/initializers/completion_kit.rb
+++ standalone/config/initializers/completion_kit.rb
@@ -1,5 +1,7 @@
 CompletionKit.configure do |config|
   config.api_token = ENV["COMPLETION_KIT_API_TOKEN"]
-  config.username = ENV.fetch("COMPLETION_KIT_USERNAME", "admin")
-  config.password = ENV["COMPLETION_KIT_PASSWORD"]
+  if ENV["COMPLETION_KIT_PASSWORD"]
+    config.username = ENV.fetch("COMPLETION_KIT_USERNAME", "admin")
+    config.password = ENV["COMPLETION_KIT_PASSWORD"]
+  end
 end
