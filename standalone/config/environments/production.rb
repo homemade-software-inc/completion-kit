b24b40d0312e268a7ec866939daeb2a42fb57d20
Damien Bastin <damien.bastin@gmail.com>
2026-03-26 16:08:20 -0700

    fix: read secret_key_base from env var in production

--- standalone/config/environments/production.rb
+++ standalone/config/environments/production.rb
@@ -1,9 +1,8 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
-  # Settings specified here will take precedence over those in config/application.rb.
+  config.secret_key_base = ENV["SECRET_KEY_BASE"]
 
-  # Code is not reloaded between requests.
   config.enable_reloading = false
 
   # Eager load code on boot for better performance and memory savings (ignored by Rake tasks).
