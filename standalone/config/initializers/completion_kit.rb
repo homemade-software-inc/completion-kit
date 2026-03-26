96453634426fa39e8476cabae6246125e766503e
Damien Bastin <damien.bastin@gmail.com>
2026-03-26 12:36:35 -0700

    fix: treat blank COMPLETION_KIT_PASSWORD as unset

--- standalone/config/initializers/completion_kit.rb
+++ standalone/config/initializers/completion_kit.rb
@@ -1,6 +1,6 @@
 CompletionKit.configure do |config|
   config.api_token = ENV["COMPLETION_KIT_API_TOKEN"]
-  if ENV["COMPLETION_KIT_PASSWORD"]
+  if ENV["COMPLETION_KIT_PASSWORD"].present?
     config.username = ENV.fetch("COMPLETION_KIT_USERNAME", "admin")
     config.password = ENV["COMPLETION_KIT_PASSWORD"]
   end
