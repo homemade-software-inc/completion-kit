861297cae443fbbe4d42cdf525f084130a68dfe7
Damien Bastin <damien.bastin@gmail.com>
2026-03-26 12:26:55 -0700

    feat: wire up LLM provider env vars in standalone initializer

--- standalone/config/initializers/completion_kit.rb
+++ standalone/config/initializers/completion_kit.rb
@@ -1,5 +1,9 @@
 CompletionKit.configure do |config|
   config.api_token = ENV["COMPLETION_KIT_API_TOKEN"]
+  config.openai_api_key = ENV["OPENAI_API_KEY"]
+  config.anthropic_api_key = ENV["ANTHROPIC_API_KEY"]
+  config.llama_api_key = ENV["LLAMA_API_KEY"]
+  config.llama_api_endpoint = ENV["LLAMA_API_ENDPOINT"]
   if ENV["COMPLETION_KIT_PASSWORD"]
     config.username = ENV.fetch("COMPLETION_KIT_USERNAME", "admin")
     config.password = ENV["COMPLETION_KIT_PASSWORD"]
